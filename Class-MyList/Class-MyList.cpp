56d84237645b465b1d3b06f5d81aeed6b9c13e82
IbragimCoder <ibragimabbasov05@gmail.com>
2023-11-24 00:50:33 +0300

    Added PushBack(),PushFront(),PopFront() ,PopBack() and Size()

--- Class-MyList/Class-MyList.cpp
+++ Class-MyList/Class-MyList.cpp
@@ -20,7 +20,95 @@ private:
     Node* last = nullptr;
 
     int elementsCount = 0;
+public:
+    void PushBack(const T& elem)
+    {
+        Node* node = new Node(elem, last, nullptr);
 
+        if (last != nullptr)
+        {
+            last->next = node;
+        }
+
+        else
+        {
+            first = node;
+        }
+
+        last = node;
+        ++elementsCount;
+    }
+
+    void PushFront(const T& elem)
+    {
+        Node* node = new Node(elem, first, nullptr);
+
+        if (first != nullptr)
+        {
+            first->prev = node;
+        }
+
+        else
+        {
+            last = node;
+        }
+
+        first = node;
+        ++elementsCount;
+
+    }
+
+    void PopBack()
+    {
+        if (last != nullptr)
+        {
+            Node* temp = last;
+            last = last->prev;
+
+            if (last != nullptr)
+            {
+                last->next = nullptr; // удаление 
+            }
+
+            else
+            {
+                first = nullptr;
+            }
+
+            delete temp;
+            elementsCount--;
+        }
+
+    }
+
+    void PopFront()
+    {
+        if (first != nullptr)
+        {
+            Node* temp = first;
+            first = first->next;
+
+            if (first != nullptr)
+            {
+                first->prev = nullptr;
+            }
+
+            else
+            {
+                last = nullptr;
+            }
+
+            delete temp;
+            elementsCount--;
+
+        }
+
+    }
+
+    size_t Size()const
+    {
+        return elementsCount;
+    }
 };
 
 int main()
